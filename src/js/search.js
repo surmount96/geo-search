55de446960bb98ec02b797ee3a2e07b3fe7ec829
surmount96 <ayanwoye74@gmail.com>
2019-11-29 18:04:42 +0100

    Update Search.js

--- src/js/search.js
+++ src/js/search.js
@@ -15,11 +15,10 @@ input.addEventListener('change', ($event) => {
     let location = $event.target.value;
     city.textContent = location.toUpperCase();
 
-    let url = 'http://api.openweathermap.org/data/2.5/weather?q=' +location +'&APPID=55fc66345d068a63d89057b952678e82';
+    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' +location +'&APPID=55fc66345d068a63d89057b952678e82';
 
     fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
+    .then(res => res.json().then((data) => {
         locationDisp.textContent = ' ' + data.sys.country;
         temp.innerHTML = convertKelvin(data.main.temp) + ' <sup>o</sup>C';
         hum.textContent = data.main.humidity;
@@ -27,7 +26,8 @@ input.addEventListener('change', ($event) => {
         ws.innerHTML = data.wind.speed + ` ms<sup>-1</sup>`;
         lat.textContent = data.coord.lat;
         lon.textContent = data.coord.lon;
-    });
+    }))
+    .catch(err => console.log(err));
 
 
 }); 
